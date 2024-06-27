3ca446eb92347ed62a28b6fcf3421e79cccec494
Egi Nurfikri <nurfikriacc23@gmail.com>
2024-06-27 09:38:08 +0700

    Initial commit

---
+++ resources/views/Produk/index.blade.php
@@ -0,0 +1,36 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Data Mahasiswa</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
+  </head>
+  <body class="bg-light">
+    <main class="container">
+        <!-- START DATA -->
+        <div class="my-3 p-3 bg-body rounded shadow-sm">
+                
+                <!-- TOMBOL TAMBAH DATA -->
+                <div class="pb-3">
+                  <a href='' class="btn btn-primary">+ Tambah Data</a>
+                </div>
+          
+                <table class="table table-striped">
+                    <thead>
+                        <tr>
+                            <th class="col-md-1">Nama Produk</th>
+                            <th class="col-md-3">Harga</th>
+                            <th class="col-md-4">Deskripsi</th>
+                            <th class="col-md-2">Tampilan</th>
+                            <th class="col-md-2">Aksi</th>
+                        </tr>
+                    </thead>
+                </table>
+               
+          </div>
+          <!-- AKHIR DATA -->
+    </main>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-u1OknCvxWvY5kfmNBILK2hRnQC3Pr17a+RTT6rIHI7NnikvbZlHgTPOOmMi466C8" crossorigin="anonymous"></script>
+  </body>
+</html>
